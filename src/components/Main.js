464727e1f59c2422bdd3b56c50d74bd32f6a60bc
Brian Huang <brian800912@gmail.com>
2018-08-24 16:23:14 -0700

    profile&shotchart

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Profile} from "./Profile";
+import {ShotChart} from "./ShotChart";
+
+export class Main extends React.Component {
+    render(){
+        return (
+            <div className='main'>
+                <Profile/>
+                <ShotChart/>
+            </div>
+        );
+    }
+
+}
