f7e8af0fa3d90499bc4b1674fa10bfe9c827034f
Brian Huang <brian800912@gmail.com>
2018-08-29 01:26:10 -0700

    fix searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import { AutoComplete, Input, Icon } from 'antd';
+import nba from 'nba';
+
+window.nba = nba;
+
+function onSelect(value) {
+    console.log('onSelect', value);
+}
 
 export class SearchBar extends React.Component {
+    state = {
+        dataSource: [],
+    }
+
+    handleSearch = (value) => {
+        this.setState({
+            dataSource: !value ? [] : nba.searchPlayers(value).map(({fullName}) => fullName),
+        });
+    }
+
+    onSelect = (value) => {
+        console.log('onSelect', value);
+    }
+
     render() {
+        const { dataSource } = this.state;
         return (
-            <div>
-                Search bar
-            </div>
+            <AutoComplete
+                className = "search-bar"
+                dataSource={dataSource}
+                size="large"
+                onSelect={this.onSelect}
+                onSearch={this.handleSearch}
+                placeholder="input here"
+            >
+                <Input suffix={<Icon type="search"  />} />
+            </AutoComplete>
         );
     }
 }
